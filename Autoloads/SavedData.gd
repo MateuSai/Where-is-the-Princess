50a0e0127b6c20ea8153316fef2e1c4e9e668566
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-07-13 19:22:50 +0200

    Modificación sistema mods

--- Autoloads/SavedData.gd
+++ Autoloads/SavedData.gd
@@ -2,14 +2,11 @@ extends Node
 
 const USER_FOLDER: String = "user://"
 const MODS_FOLDER_NAME: String = "mods/"
-const ROOMS_FOLDER_NAME: String = "rooms/"
-const ARMORS_FOLDER_NAME: String = "armors/"
-const WEAPONS_FOLDER_NAME: String = "weapons/"
 
 var run_stats: RunStats = RunStats.new()
 
 var mods: Dictionary = {
-	rooms = [],
+
 }
 
 
@@ -18,34 +15,25 @@ func _ready() -> void:
 	assert(user_dir) # Siempre deberiamos poder abrir el directorio del usuario
 
 	if not user_dir.dir_exists(MODS_FOLDER_NAME):
-		print("Can't find mods folder, creating it along his subfolders...")
-		for sub_folder in [ROOMS_FOLDER_NAME, ARMORS_FOLDER_NAME, WEAPONS_FOLDER_NAME]:
-			if user_dir.make_dir_recursive(MODS_FOLDER_NAME + sub_folder):
-				printerr("Error creating " + sub_folder + "!")
-	else:
-		for sub_folder in [ROOMS_FOLDER_NAME, ARMORS_FOLDER_NAME, WEAPONS_FOLDER_NAME]:
-			if not user_dir.dir_exists(MODS_FOLDER_NAME + sub_folder):
-				print("Can't find the " + sub_folder + " directory, creating it...")
-				if user_dir.make_dir_recursive(MODS_FOLDER_NAME + sub_folder):
-					printerr("Error creating " + sub_folder + "!")
+		print("Can't find mods folder, creating it...")
+		if user_dir.make_dir(MODS_FOLDER_NAME):
+			printerr("Error creating mods directory!!")
+			return
 
-	for room_path in _get_mod_room_paths():
-		mods.rooms.push_back(Mod.new(room_path))
+	var mods_dir: DirAccess = DirAccess.open(USER_FOLDER + MODS_FOLDER_NAME)
+	if mods_dir == null:
+		printerr("Error opening mods directory!")
+		return
 
+	for file_name in mods_dir.get_files():
+		print(file_name)
+		mods[file_name] = USER_FOLDER + MODS_FOLDER_NAME + file_name
+		#var room: PackedScene = load(USER_FOLDER + ROOMS_FOLDER_NAME + file_name)
+		#custom_rooms.push_back(room)
+		#room_paths.push_back(USER_FOLDER + ROOMS_FOLDER_NAME + file_name)
 
-func _get_mod_room_paths() -> Array:
-	var room_paths: Array = []
-	var rooms_folder: DirAccess = DirAccess.open(USER_FOLDER + MODS_FOLDER_NAME + ROOMS_FOLDER_NAME)
-	if not rooms_folder:
-		printerr("Error opening rooms folder!!")
-	else:
-		for file_name in rooms_folder.get_files():
-			#print(file_name)
-			#var room: PackedScene = load(USER_FOLDER + ROOMS_FOLDER_NAME + file_name)
-			#custom_rooms.push_back(room)
-			room_paths.push_back(USER_FOLDER + ROOMS_FOLDER_NAME + file_name)
-
-	return room_paths
+#	for room_path in _get_mod_room_paths():
+#		mods.rooms.push_back(Mod.new(room_path))
 
 
 func reset_data() -> void:
