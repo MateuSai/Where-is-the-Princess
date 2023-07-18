625373b923dd9335574633b5e8942a8dc9102fc9
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-07-18 15:48:50 +0200

    Function that checks if we can pick item

--- Items/ItemOnFloor.gd
+++ Items/ItemOnFloor.gd
@@ -1,12 +1,25 @@
 class_name ItemOnFloor extends Sprite2D
 
 var item: Item
+var can_pick_up: bool = false
 
 @onready var interact_area: InteractArea = get_node("InteractArea")
 
 
+func _ready() -> void:
+	interact_area.body_entered.connect(func(player: Player):
+		can_pick_up = item.can_pick_up(player)
+		if can_pick_up:
+			interact_area.sprite_material.set("shader_parameter/color", Color.WHITE)
+		else:
+			interact_area.sprite_material.set("shader_parameter/color", Color.RED)
+	)
+
+
 func enable_pick_up() -> void:
 	interact_area.player_interacted.connect(func():
+		if not can_pick_up:
+			return
 		item.pick_up(interact_area.player)
 		free()
 	)
--- Items/Whetstone.gd
+++ Items/Whetstone.gd
@@ -5,5 +5,9 @@ func _init() -> void:
 	_initialize(load("res://Art/Status Conditions/condition_status_top_fire_full_16x16.png"))
 
 
+func can_pick_up(player: Player) -> bool:
+	return player.weapons.current_weapon.get_index() > 0 and player.weapons.current_weapon.stats.condition < 100
+
+
 func pick_up(player: Player) -> void:
 	player.weapons.current_weapon.stats.set_condition(100)
