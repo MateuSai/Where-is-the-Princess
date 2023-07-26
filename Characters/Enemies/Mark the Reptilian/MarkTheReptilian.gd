7eb42706dc3888b7e87e67b218b458f5c2ab01eb
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-07-26 12:52:01 +0200

    (:|

--- Characters/Enemies/Mark the Reptilian/MarkTheReptilian.gd
+++ Characters/Enemies/Mark the Reptilian/MarkTheReptilian.gd
@@ -1,40 +1,57 @@
 extends Enemy
 
 
-@onready var weapon: Node2D = $Weapon
-@onready var weapon_body: RigidBodyWeapon = $Weapon/RigidBody2D
-@onready var rope: Node2D = $Weapon/Rope
-@onready var weapon_joint: PinJoint2D = $Weapon/Rope/PinJoint2D
+var spear_and_rope: SpearAndRope
+
+@onready var spear_pivot: Node2D = $SpearPivot
+@onready var spear_sprite: Sprite2D = spear_pivot.get_node("SpearSprite")
+#@onready var weapon: Node2D = $Weapon
+#@onready var weapon_body: RigidBodyWeapon = $Weapon/RigidBody2D
+#@onready var rope: Node2D = $Weapon/Rope
+#@onready var weapon_joint: PinJoint2D = $Weapon/Rope/PinJoint2D
 
 
 func _ready() -> void:
+	super()
 	#weapon_body.process_mode = Node.PROCESS_MODE_DISABLED
-	weapon_body.position = Vector2.DOWN * 8
-	weapon_body.can_move = false
-	rope.hide()
+#	weapon_body.position = Vector2.DOWN * 8
+#	weapon_body.can_move = false
+#	rope.hide()
 
 
 
 func point_to_player() -> void:
-	pass
+	var vector_to_target: Vector2 = (player.position - global_position)
+	spear_pivot.rotation = vector_to_target.angle()
+	if vector_to_target.x > 0:
+		spear_pivot.scale.x = 1
+	else:
+		spear_pivot.scale.x = -1
+
 	#weapon.rotation = (player.position - global_position).angle()
 
 
 func attack() -> void:
+	spear_and_rope = load("res://Characters/Enemies/Mark the Reptilian/SpearAndRope.tscn").instantiate()
+	get_tree().current_scene.add_child(spear_and_rope)
+	spear_and_rope.position = global_position
+	spear_and_rope.attach(get_path(), (player.position - global_position).normalized())
 	#weapon.rotation = (player.position - global_position).angle()
-	rope.show()
+	#rope.show()
 	#$Weapon.rotation = (player.position - global_position).angle() - PI/2
-	weapon_body.can_move = true
+	#weapon_body.can_move = true
 	#weapon_joint.node_a = weapon_joint.get_path_to(weapon_body)
-	weapon_body.apply_impulse((player.position - weapon.global_position).normalized() * 1500)
+	#weapon_body.apply_impulse((player.position - weapon.global_position).normalized() * 1500)
 
 
 func pull_back_weapon() -> void:
+	if is_instance_valid(spear_and_rope):
+		spear_and_rope.queue_free()
 	#weapon_body.linear_velocity = Vector2.ZERO
 	#weapon_body.freeze = true
 	#rope.hide()
 	#weapon_joint.node_a = ""
-	weapon_body.can_move = false
+	#weapon_body.can_move = false
 	#weapon_body.set_pos(global_position + Vector2.DOWN * 8)
 	#weapon_body.position = Vector2.DOWN * 8
 	#weapon_body.global_position = global_position + Vector2.DOWN * 8
--- Characters/Enemies/Mark the Reptilian/RigidBodyWeapon.gd
+++ Characters/Enemies/Mark the Reptilian/RigidBodyWeapon.gd
@@ -4,20 +4,8 @@ class_name RigidBodyWeapon extends RigidBody2D
 var change_position: bool = false
 var new_pos: Vector2
 
-var can_move: bool = true
-
-
-func _ready() -> void:
-	add_collision_exception_with(owner)
-
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	if not can_move:
-		$Sprite2D.rotation = (owner.player.position - owner.global_position).angle()
-		$CollisionShape2D.rotation = (owner.player.position - owner.global_position).angle() - PI/2
-		state.linear_velocity = Vector2.ZERO
-		state.transform.origin = owner.global_position + (Vector2.RIGHT * 8).rotated((owner.player.position - owner.global_position).angle())
-
 	if change_position:
 		state.transform.origin = new_pos
 		state.linear_velocity = Vector2.ZERO
