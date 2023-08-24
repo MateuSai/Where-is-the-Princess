76876508df0e52ac8609cdc874df93b885ac4407
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-08-24 18:47:15 +0200

    Armor ability shine effect

--- ui/ModsButton.gd
+++ ui/ModsButton.gd
@@ -2,4 +2,5 @@ extends Button
 
 
 func _ready() -> void:
-	pressed.connect(func(): owner.add_child(ModMenu.new()))
+	#pressed.connect(func(): owner.add_child(ModMenu.new()))
+	pressed.connect(func(): get_tree().root.get_node("UserProfiles").popup_centered())
