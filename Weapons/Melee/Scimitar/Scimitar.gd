4737d54d9030d92f356262de591aceb60e261734
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-08-30 12:22:58 +0200

    Scimiter ability adjustments

--- Weapons/Melee/Scimitar/Scimitar.gd
+++ Weapons/Melee/Scimitar/Scimitar.gd
@@ -1,12 +1,11 @@
 extends MeleeWeapon
 
 
-const PROJECTILE_SCENE: PackedScene = preload("res://Weapons/Melee/Scimitar/ScimitarProjectile.tscn")
+const EFFECT_SCENE: PackedScene = preload("res://Weapons/Melee/Scimitar/ScimitarEffect.tscn")
 
 
-func _throw_projectile() -> void:
-	var projectile: Projectile = PROJECTILE_SCENE.instantiate()
-#	hitbox.exclude.push_back(projectile)
-	get_tree().current_scene.add_child(projectile)
-	var dir: Vector2 = Vector2.RIGHT.rotated(rotation + randf_range(-0.25, 0.25))
-	projectile.launch(global_position + dir * 16, dir, 150, true)
+func _spawn_effect() -> void:
+	var effect: Sprite2D = EFFECT_SCENE.instantiate()
+	effect.rotation = rotation + randf_range(-0.25, 0.25)
+	effect.position = global_position + Vector2.RIGHT.rotated(effect.rotation) * 16
+	get_tree().current_scene.add_child(effect)
