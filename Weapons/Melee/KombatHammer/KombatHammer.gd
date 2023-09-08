afec33dbc61519d1611543055128fe518874c241
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-09-08 20:14:37 +0200

    Started working on kombat hammer active ability

---
+++ Weapons/Melee/KombatHammer/KombatHammer.gd
@@ -0,0 +1,7 @@
+extends MeleeWeapon
+
+const QUAKE_PROJECTILE_SCENE: PackedScene = preload("res://Weapons/Melee/KombatHammer/KombatHammerQuake.tscn")
+
+
+func _spawn_quake_projectiles() -> void:
+	pass
