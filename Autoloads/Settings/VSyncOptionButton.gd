b9771d05d16fd58923e24a9131dbab77bef70416
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-09-10 18:11:06 +0200

    Trying to fix kombat hammer

--- Autoloads/Settings/VSyncOptionButton.gd
+++ Autoloads/Settings/VSyncOptionButton.gd
@@ -5,7 +5,7 @@ func _ready() -> void:
 	add_item("DISABLED")
 	add_item("ENABLED")
 	add_item("ADAPTATIVE")
-	add_item("MAILBOX")
+	#add_item("MAILBOX")
 	select(DisplayServer.window_get_vsync_mode())
 	item_selected.connect(func(index: int):
 		DisplayServer.window_set_vsync_mode(index)
