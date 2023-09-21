35b8135812b217826440cf1403a167f143a16b1b
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-09-21 20:15:34 +0200

    sfsdfsdfhdsjf

--- mods-unpacked/GodotModding-UserProfile/content/UserProfiles.gd
+++ mods-unpacked/GodotModding-UserProfile/content/UserProfiles.gd
@@ -3,20 +3,20 @@ class_name UserProfilesPopup
 
 
 @export var user_profile_section: PackedScene
-@export var text_select_profile := "Select Profile"
-@export var text_restart := "A game restart is required to apply the settings"
-@export var text_profile_create_error := "There was an error creating the profile - check logs"
-@export var text_profile_select_error := "There was an error selecting the profile - check logs"
-@export var text_profile_delete_error := "There was an error deleting the profile - check logs"
-@export var text_mod_enable_error := "There was an error enabling the mod - check logs"
-@export var text_mod_disable_error := "There was an error disabling the mod - check logs"
-@export var text_mod_current_config_change_error := "There was an error changing the config - check logs"
-@export var text_current_profile := " (Current Profile)"
+#@export var text_select_profile := "Select Profile"
+const TEXT_RESTART := "A game restart is required to apply the settings"
+const TEXT_PROFILE_CREATE_ERROR := "There was an error creating the profile - check logs"
+const TEXT_PROFILE_SELECT_ERROR := "There was an error selecting the profile - check logs"
+const TEXT_PROFILE_DELETE_ERROR := "There was an error deleting the profile - check logs"
+const TEXT_MOD_ENABLE_ERROR := "There was an error enabling the mod - check logs"
+const TEXT_MOD_DISABLE_ERROR := "There was an error disabling the mod - check logs"
+const TEXT_MOD_CURRENT_CONFIG_CHANGE_ERROR := "There was an error changing the config - check logs"
+const TEXT_CURRENT_PROFILE := " (Current Profile)"
 
 # I can't put a material on the PopupPanel, so I'm using the material of his panel.
 # It seems it always have the same name, you can check it on the remote tab
 # and change it if it's different on your case
-@onready var panel: Panel = $"@Panel@16"
+#@onready var panel: Panel = $"@Panel@16"
 @onready var label_select_profile: Label = $"%LabelSelectProfile"
 @onready var user_profile_sections: VBoxContainer = $"%UserProfileSections"
 @onready var profile_select: OptionButton = $"%ProfileSelect"
@@ -28,8 +28,8 @@ class_name UserProfilesPopup
 
 
 func _ready() -> void:
-	panel.material = ShaderMaterial.new()
-	panel.material.shader = load("res://mods-unpacked/GodotModding-UserProfile/assets/shader/blur.tres")
+#	panel.material = ShaderMaterial.new()
+#	panel.material.shader = load("res://mods-unpacked/GodotModding-UserProfile/assets/shader/blur.tres")
 
 	_populate_profile_select()
 	_generate_user_profile_section()
@@ -50,15 +50,16 @@ func _ready() -> void:
 
 
 func apply_config(config: ModConfig) -> void:
-	label_select_profile.text = config.data.select_profile_text
-
-	var material_settings: Dictionary = config.data.material_settings
-
-	panel.material.set_shader_parameter("animate", material_settings.animate)
-	panel.material.set_shader_parameter("square_scale", material_settings.square_scale)
-	panel.material.set_shader_parameter("blur_amount", material_settings.blur_amount)
-	panel.material.set_shader_parameter("mix_amount", material_settings.mix_amount)
-	panel.material.set_shader_parameter("color_over", Color(material_settings.color))
+	return
+#	label_select_profile.text = config.data.select_profile_text
+#
+#	var material_settings: Dictionary = config.data.material_settings
+#
+#	panel.material.set_shader_parameter("animate", material_settings.animate)
+#	panel.material.set_shader_parameter("square_scale", material_settings.square_scale)
+#	panel.material.set_shader_parameter("blur_amount", material_settings.blur_amount)
+#	panel.material.set_shader_parameter("mix_amount", material_settings.mix_amount)
+#	panel.material.set_shader_parameter("color_over", Color(material_settings.color))
 
 
 func _update_ui() -> void:
@@ -76,7 +77,7 @@ func _populate_profile_select() -> void:
 
 	for user_profile in ModLoaderUserProfile.get_all_as_array():
 		var is_current_profile := true if ModLoaderUserProfile.get_current().name == user_profile.name else false
-		profile_select.add_item(user_profile.name + text_current_profile if is_current_profile else user_profile.name)
+		profile_select.add_item(user_profile.name + TEXT_CURRENT_PROFILE if is_current_profile else user_profile.name)
 
 		# Get the item index of the current profile
 		if is_current_profile:
@@ -100,11 +101,11 @@ func _on_ButtonDeleteProfile_pressed():
 	var profile_to_delete := ModLoaderStore.current_user_profile
 	# Switch to default profile
 	if not ModLoaderUserProfile.set_profile(ModLoaderUserProfile.get_profile("default")):
-		info_text.text = text_profile_select_error
+		info_text.text = TEXT_PROFILE_SELECT_ERROR
 		return
 	# Delete the profile
 	if not ModLoaderUserProfile.delete_profile(profile_to_delete):
-		info_text.text = text_profile_delete_error
+		info_text.text = TEXT_PROFILE_DELETE_ERROR
 		return
 
 	_update_ui()
@@ -115,7 +116,7 @@ func _on_ButtonProfileNameSubmit_pressed() -> void:
 	if not ModLoaderUserProfile.create_profile(input_profile_name.text):
 		# If there was an error creating the profile
 		# Add error message to the info text label
-		info_text.text = text_profile_create_error
+		info_text.text = TEXT_PROFILE_CREATE_ERROR
 		# And return early
 		return
 
@@ -128,32 +129,32 @@ func _on_ButtonProfileNameSubmit_pressed() -> void:
 func _on_ProfileSelect_item_selected(index: int) -> void:
 	var user_profile := ModLoaderUserProfile.get_profile(profile_select.get_item_text(index))
 	if not ModLoaderUserProfile.set_profile(user_profile):
-		info_text.text = text_profile_select_error
+		info_text.text = TEXT_PROFILE_SELECT_ERROR
 		return
 
 	_update_ui()
 
-	info_text.text = text_restart
+	info_text.text = TEXT_RESTART
 
 
 func _on_ModList_mod_is_active_changed(mod_id: String, is_active: bool)  -> void:
 	if is_active:
 		if not ModLoaderUserProfile.enable_mod(mod_id):
-			info_text.text = text_mod_enable_error
+			info_text.text = TEXT_MOD_ENABLE_ERROR
 			return
 	else:
 		if not ModLoaderUserProfile.disable_mod(mod_id):
-			info_text.text = text_mod_disable_error
+			info_text.text = TEXT_MOD_DISABLE_ERROR
 			return
 
-	info_text.text = text_restart
+	info_text.text = TEXT_RESTART
 
 
 func _on_ModList_mod_current_config_changed(mod_id: String, current_config_name: String):
 	var config := ModLoaderConfig.get_config(mod_id, current_config_name)
 
 	if not config:
-		info_text.text = text_mod_current_config_change_error
+		info_text.text = TEXT_MOD_CURRENT_CONFIG_CHANGE_ERROR
 
 	ModLoaderConfig.set_current_config(config)
 
