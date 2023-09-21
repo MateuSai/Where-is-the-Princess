ea7e99b0b7b5e3bf5a610a98fd7f1af37e4dc5cf
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-09-21 12:37:36 +0200

    dsfdsf

--- Characters/Enemies/SpearTribal/SpearTribalFSM.gd
+++ Characters/Enemies/SpearTribal/SpearTribalFSM.gd
@@ -3,6 +3,7 @@ extends FiniteStateMachine
 @onready var spear_hitbox_collision_shape: CollisionShape2D = $"../SpearPivot/Sprite2D/Hitbox/CollisionShape2D"
 @onready var charge_raycast: RayCast2D = $"../SpearPivot/Sprite2D/ChargeRayCast"
 @onready var spear_animation_player: AnimationPlayer = $"../SpearPivot/AnimationPlayer"
+@onready var aim_raycast: RayCast2D = $"../AimRayCast"
 #@onready var attack_animation_leftover_timer: Timer = $"../AttackAnimationLeftoverTimer"
 
 
@@ -19,15 +20,24 @@ func _ready() -> void:
 
 
 func _state_logic(_delta: float) -> void:
-	parent.point_to_player()
 	match state:
 		states.move:
+			parent.point_to_player()
 			parent.chase()
 			parent.move()
 			if parent.mov_direction.y >= 0 and animation_player.current_animation != "move":
 				animation_player.play("move")
 			elif parent.mov_direction.y < 0 and animation_player.current_animation != "move_up":
 				animation_player.play("move_up")
+		states.attack:
+			parent.point_to_player()
+		states.charge:
+			parent.move()
+			if parent.mov_direction.y >= 0 and animation_player.current_animation != "move":
+				animation_player.play("move")
+			elif parent.mov_direction.y < 0 and animation_player.current_animation != "move_up":
+				animation_player.play("move_up")
+
 #		states.attack:
 #			hitbox.rotation = (parent.player.position - parent.global_position).angle()
 #			hitbox.knockback_direction = Vector2.RIGHT.rotated(hitbox.rotation)
@@ -46,7 +56,9 @@ func _get_transition() -> int:
 				else:
 					return states.attack
 		states.move:
-			if parent.distance_to_player < parent.MAX_DISTANCE_TO_PLAYER and parent.distance_to_player > parent.MIN_DISTANCE_TO_PLAYER:
+			if not aim_raycast.is_colliding() and parent.distance_to_player > parent.MIN_DISTANCE_TO_CHARGE:
+				return states.charge
+			elif parent.distance_to_player < parent.MAX_DISTANCE_TO_PLAYER and parent.distance_to_player > parent.MIN_DISTANCE_TO_PLAYER:
 				if not spear_animation_player.is_playing():
 					return states.attack
 				else:
@@ -75,7 +87,9 @@ func _enter_state(_previous_state: int, new_state: int) -> void:
 				animation_player.play("idle_up")
 			spear_animation_player.play("attack")
 		states.charge:
-			parent.max_speed = 150
+			parent.max_speed = 500
+			parent.mov_direction = (parent.player.position - parent.global_position).normalized()
+			spear_animation_player.play("charge")
 		states.dead:
 			pass
 			# parent.spawn_loot()
@@ -87,5 +101,6 @@ func _exit_state(state_exited: int) -> void:
 		states.attack:
 			spear_animation_player.play("restore")
 		states.charge:
-			parent.max_speed = 50
+			parent.max_speed = 70
 			spear_hitbox_collision_shape.disabled = true
+			spear_animation_player.stop()
