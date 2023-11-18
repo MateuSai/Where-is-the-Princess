f97ed11ef7bcd440fe92fd4176f27a02f9caafbe
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-11-18 18:31:34 +0100

    Wait time before skelebro starts attacking

--- Characters/Enemies/enemy_weapons.gd
+++ Characters/Enemies/enemy_weapons.gd
@@ -1,5 +1,6 @@
 class_name EnemyWeapons extends Weapons
 
+var pre_attack_cooldown_timer: Timer
 var attack_cooldown_timer: Timer
 
 
@@ -8,6 +9,12 @@ func _ready() -> void:
 
 	current_weapon = get_child(0)
 
+	pre_attack_cooldown_timer = Timer.new()
+	pre_attack_cooldown_timer.one_shot = true
+	pre_attack_cooldown_timer.wait_time = 0.3
+	pre_attack_cooldown_timer.timeout.connect(_actually_attack)
+	add_child(pre_attack_cooldown_timer)
+
 	attack_cooldown_timer = Timer.new()
 	attack_cooldown_timer.one_shot = true
 	attack_cooldown_timer.wait_time = 0.75
@@ -15,12 +22,16 @@ func _ready() -> void:
 
 
 func is_busy() -> bool:
-	return current_weapon.is_busy() or not attack_cooldown_timer.is_stopped()
+	return current_weapon.is_busy() or not attack_cooldown_timer.is_stopped() or not pre_attack_cooldown_timer.is_stopped()
 
 
 func attack() -> void:
 	assert(not is_busy())
 
+	pre_attack_cooldown_timer.start()
+
+
+func _actually_attack() -> void:
 	current_weapon._attack()
 
 	attack_cooldown_timer.start()
