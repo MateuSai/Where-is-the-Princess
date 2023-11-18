7657daff148440bb0e8c3816f67e571710b2da05
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-11-18 10:55:19 +0100

    Enemy melee attack cooldown

--- Characters/Enemies/enemy_weapons.gd
+++ Characters/Enemies/enemy_weapons.gd
@@ -1,6 +1,17 @@
 class_name EnemyWeapons extends Node2D
 
+var attack_cooldown_timer: Timer
+
 @onready var current_weapon: Weapon = get_child(0)
+
+
+func _ready() -> void:
+#	super()
+
+	attack_cooldown_timer = Timer.new()
+	attack_cooldown_timer.one_shot = true
+	attack_cooldown_timer.wait_time = 0.75
+	add_child(attack_cooldown_timer)
 
 
 func move(mouse_direction: Vector2) -> void:
@@ -17,10 +28,12 @@ func move(mouse_direction: Vector2) -> void:
 
 
 func is_busy() -> bool:
-	return current_weapon.is_busy()
+	return current_weapon.is_busy() or not attack_cooldown_timer.is_stopped()
 
 
 func attack() -> void:
 	assert(not is_busy())
 
 	current_weapon._attack()
+
+	attack_cooldown_timer.start()
