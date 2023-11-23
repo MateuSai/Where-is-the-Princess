63b381752131bcca38372a48dd2cfe14cf95f123
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-11-23 18:26:50 +0100

    Start game corridor

--- BaseCamp.gd
+++ BaseCamp.gd
@@ -1,7 +1,6 @@
-extends Node2D
+class_name BaseCamp extends Node2D
 
 @onready var player: Player = get_node("Player")
-@onready var start_interact_area: InteractArea = get_node("StartInteractArea")
 @onready var wardrobe_interact_area: InteractArea = get_node("Wardrobe/WardrobeInteractArea")
 @onready var settings_interact_area: InteractArea = $Settings/SettingsInteractArea
 #@onready var mods_interact_area: InteractArea = $Mods/ModsInteractArea
@@ -15,20 +14,6 @@ extends Node2D
 
 func _ready() -> void:
 	player.set_armor(load(SavedData.data.equipped_armor).new())
-
-	start_interact_area.player_interacted.connect(func():
-		var seed_spin_box: SpinBox = seed_popup.get_node("MarginContainer/SpinBox")
-		var run_seed: int
-		if seed_spin_box.value == -1:
-			run_seed = randi()
-		else:
-			run_seed = int(seed_spin_box.value)
-		#run_seed = 2629861273
-		print("Seed: " + str(run_seed) + "\n")
-		seed(run_seed)
-		Globals.run_seed = run_seed
-		SceneTransistor.start_transition_to("res://Game.tscn")
-	)
 
 	settings_interact_area.player_interacted.connect(func():
 		Settings.popup_centered()
@@ -70,3 +55,17 @@ func _ready() -> void:
 
 func _exit_tree() -> void:
 	SavedData.save_data()
+
+
+func start_game() -> void:
+	var seed_spin_box: SpinBox = seed_popup.get_node("MarginContainer/SpinBox")
+	var run_seed: int
+	if seed_spin_box.value == -1:
+		run_seed = randi()
+	else:
+		run_seed = int(seed_spin_box.value)
+	#run_seed = 2629861273
+	print("Seed: " + str(run_seed) + "\n")
+	seed(run_seed)
+	Globals.run_seed = run_seed
+	SceneTransistor.start_transition_to("res://Game.tscn")
---
+++ start_area.gd
@@ -0,0 +1,16 @@
+class_name StartArea extends Area2D
+
+@onready var base_camp: BaseCamp = owner
+@onready var color_rect: ColorRect = %ColorRect
+
+
+func _ready() -> void:
+	body_exited.connect(func(body: Node2D):
+		assert(body is Player)
+		if body.position.y > position.y:
+			color_rect.color.a = clamp(color_rect.color.a - 0.25, 0.0, 1.0)
+		else:
+			color_rect.color.a = clamp(color_rect.color.a + 0.25, 0.0, 1.0)
+			if color_rect.color.a == 1.0:
+				base_camp.start_game()
+	)
