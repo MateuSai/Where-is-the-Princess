afd216f0f6c77309a8effa2d72e38dea21ef47a7
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-12-05 07:18:53 +0100

    Explosion when crate is destroyed

--- Rooms/Furniture and Traps/crate.gd
+++ Rooms/Furniture and Traps/crate.gd
@@ -1,5 +1,6 @@
 class_name Crate extends StaticBody2D
 
+const EXPLOSION_SCENE: PackedScene = preload("res://Characters/Enemies/SpawnExplosion.tscn")
 
 @onready var room: DungeonRoom = get_parent()
 
@@ -10,6 +11,9 @@ class_name Crate extends StaticBody2D
 func _ready() -> void:
 	life_component.died.connect(func() -> void:
 		sprite.frame = (randi() % 3) + 1
+		var explosion: AnimatedSprite2D = EXPLOSION_SCENE.instantiate()
+		explosion.position = global_position
+		get_tree().current_scene.add_child(explosion)
 		# Now the flying units will be able to pass over the box
 		collision_layer = 16 # Low object
 		remove_from_group(DungeonRoom.FLYING_UNITS_NAVIGATION_GROUP)
