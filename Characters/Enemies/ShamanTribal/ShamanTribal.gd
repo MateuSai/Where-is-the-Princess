23a365483899bc74b1ec498f97bfe2c6c48b48c2
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-12-12 17:40:08 +0100

    Fixed bomb tribal

--- Characters/Enemies/ShamanTribal/ShamanTribal.gd
+++ Characters/Enemies/ShamanTribal/ShamanTribal.gd
@@ -8,7 +8,7 @@ var target_tribal_mask: TribalMask
 func _ready() -> void:
 	super()
 
-	search_tribal_mask_timer.timeout.connect(func():
+	search_tribal_mask_timer.timeout.connect(func() -> void:
 		self.target_tribal_mask = get_closer_tribal_mask()
 	)
 
@@ -17,7 +17,7 @@ func get_closer_tribal_mask() -> TribalMask:
 	var closer_tribal_mask: TribalMask = null
 	var distance_to_closer_tribal_mask: float = INF
 
-	for tribal_mask in get_tree().get_nodes_in_group("tribal_masks"):
+	for tribal_mask: TribalMask in get_tree().get_nodes_in_group("tribal_masks"):
 		var dis: float = (tribal_mask.global_position - global_position).length()
 		if dis < distance_to_closer_tribal_mask:
 			closer_tribal_mask = tribal_mask
