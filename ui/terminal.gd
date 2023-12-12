9798f9cf1e148945f8fc5cd02b01f4c9b00d60e6
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-12-12 18:39:09 +0100

    Set armor using id

--- ui/terminal.gd
+++ ui/terminal.gd
@@ -214,18 +214,27 @@ func _set_player_hp(hp_string: String) -> void:
 
 
 func _set_armor(armor_string: String) -> void:
-	if not FileAccess.file_exists(armor_string):
-		printerr("Error: There is no file at " + armor_string)
-		return
+	var armor_path: String
 
-	var armor_script: GDScript = load(armor_string)
+	if armor_string.is_absolute_path():
+		if not FileAccess.file_exists(armor_string):
+			printerr("Error: There is no file at " + armor_string)
+			return
+		armor_path = armor_string
+	else:
+		armor_path = Armor.id_to_path(armor_string)
+		if armor_path.is_empty():
+			printerr("Error: could not find armor with id " + armor_string)
+			return
+
+	var armor_script: GDScript = load(armor_path)
 	if not armor_script:
 		printerr("Error: There is no script at the specified path")
 		return
 
 	var script_instance: Object = armor_script.new()
 	if not script_instance is Armor:
-		printerr("Error: " + armor_string + " does not extend Armor")
+		printerr("Error: " + armor_path + " does not extend Armor")
 		return
 
 	Globals.player.set_armor(script_instance as Armor)
