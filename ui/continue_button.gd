f57ddd334beaebbea9acfc8c7edbb1e80e2780e5
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-12-16 13:11:41 +0100

    Title

--- ui/continue_button.gd
+++ ui/continue_button.gd
@@ -1,11 +1,13 @@
 class_name ContinueButton extends ButtonWithSound
 
 
-func _ready() -> void:
-	super()
-
+func _init() -> void:
 	if not SavedData.are_there_run_stats():
 		queue_free()
+
+
+func _ready() -> void:
+	super()
 
 	pressed.connect(func() -> void:
 		SceneTransistor.start_transition_to("res://Game.tscn")
