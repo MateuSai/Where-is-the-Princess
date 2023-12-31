e4d15ee4d39ac075e2e26b34d96dadd95f0d13c6
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-12-31 19:05:20 +0100

    sjdfdsbf

--- Rooms/Furniture and Traps/crocodile_trap/crocodile_trap.gd
+++ Rooms/Furniture and Traps/crocodile_trap/crocodile_trap.gd
@@ -3,39 +3,45 @@ class_name CrocodileTrap extends Area2D
 const ATTACK_RANGE: int = 16
 const BITE_EFFECT_SCENE: PackedScene = preload("res://Characters/Enemies/BiteEffect.tscn")
 
+@export var knockback_dir: Vector2
+
 @onready var sprite: Sprite2D = $Sprite2D
-@onready var range_shape: CircleShape2D = $CollisionShape2D.shape
-@onready var range_radius: float = range_shape.radius
+#@onready var range_shape: CircleShape2D = $CollisionShape2D.shape
+#@onready var range_radius: float = range_shape.radius
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var hitbox: Hitbox = $Hitbox
 @onready var hitbox_col: CollisionShape2D = $Hitbox/CollisionShape2D
 
 
 func _ready() -> void:
-	set_process(false)
+	#set_process(false)
 	sprite.frame = 0
 
 	body_entered.connect(func(body: Node2D) -> void:
 		if body is Player:
-			set_process(true)
+			#set_process(true)
+			animation_player.clear_queue()
+			animation_player.play("bite")
 	)
 	body_exited.connect(func(body: Node2D) -> void:
 		if body is Player:
-			set_process(false)
+			#set_process(false)
+			animation_player.clear_queue()
+			animation_player.play("idle")
 	)
 
-	hitbox.knockback_direction = Vector2.UP
+	hitbox.knockback_direction = knockback_dir
 
 
-func _process(_delta: float) -> void:
-	if animation_player.is_playing():
-		return
-
-	var distance_to_player: float = (Globals.player.global_position - global_position).length()
-	if distance_to_player <= ATTACK_RANGE:
-		animation_player.play("bite_up")
-	else:
-		sprite.frame = round((1 - ((distance_to_player - ATTACK_RANGE) / (range_radius - ATTACK_RANGE))) * 6)
+#func _process(_delta: float) -> void:
+	#if animation_player.is_playing():
+		#return
+#
+	#var distance_to_player: float = (Globals.player.global_position - global_position).length()
+	#if distance_to_player <= ATTACK_RANGE:
+		#animation_player.play("bite_up")
+	#else:
+		#sprite.frame = round((1 - ((distance_to_player - ATTACK_RANGE) / (range_radius - ATTACK_RANGE))) * 6)
 
 
 func _spawn_bite_effect() -> void:
