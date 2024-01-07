a4cd97f4a2d385a2de87076df0432c593503a2f8
Mateu <57108338+MateuSai@users.noreply.github.com>
2024-01-07 11:24:57 +0100

    Joe movement and crossbow and tromp limit to 4 skeletons

--- Characters/Enemies/crocodile_joe/crocodile_joe_fsm.gd
+++ Characters/Enemies/crocodile_joe/crocodile_joe_fsm.gd
@@ -1,11 +1,50 @@
 class_name CrocodileJoeFSM extends FiniteStateMachine
 
 enum {
-	ATTACK,
-	FLEE,
+	LONG_RANGE,
+	CLOSE_RANGE,
 	DEAD,
 }
 
+const DISTANCE_TO_CHANGE_RANGE: int = 10
+
+@onready var joe: CrocodileJoe = get_parent()
+@onready var pathfinding_component: PathfindingComponent = $"../PathfindingComponent"
+@onready var weapons: EnemyWeapons = $"../EnemyWeapons"
+
 
 func start() -> void:
-	set_state(ATTACK)
+	set_state(LONG_RANGE)
+
+
+func _state_logic(_delta: float) -> void:
+	match state:
+		LONG_RANGE:
+			weapons.move((joe.target.global_position - joe.global_position).normalized())
+			if not weapons.is_busy():
+				if weapons.current_weapon.weapon_sprite.frame == Crossbow.RELEASED_FRAME:
+					weapons.reload()
+				else:
+					weapons.attack()
+
+			joe.move_to_target()
+			joe.move()
+
+
+func _get_transition() -> int:
+	match state:
+		LONG_RANGE:
+			var distance_to_target: float = (joe.target.global_position - joe.global_position).length()
+			if distance_to_target < DISTANCE_TO_CHANGE_RANGE:
+				return CLOSE_RANGE
+		CLOSE_RANGE:
+			var distance_to_target: float = (joe.target.global_position - joe.global_position).length()
+			if distance_to_target > DISTANCE_TO_CHANGE_RANGE:
+				return LONG_RANGE
+	return -1
+
+
+func _enter_state(_previous_state: int, new_state: int) -> void:
+	match new_state:
+		LONG_RANGE:
+			pathfinding_component.set_mode(PathfindingComponent.Wander.new())
