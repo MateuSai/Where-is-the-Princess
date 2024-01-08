c09b590adc40bf3dc28c8ba76b5c4a1a6c54c1e3
Mateu <57108338+MateuSai@users.noreply.github.com>
2024-01-08 08:33:45 +0100

    Test to check weapons and armors on data.gd are valid

---
+++ Weapons/Melee/spear/spear.gd
@@ -0,0 +1,13 @@
+class_name Spear extends MeleeWeapon
+
+const ABILITY_EFFECT: PackedScene = preload("res://Weapons/Melee/spear/spear_ability_effect.tscn")
+
+
+func _add_ability_effect() -> void:
+	var effect: Hitbox = ABILITY_EFFECT.instantiate()
+	effect.damage = ability_damage
+	effect.knockback_force = ability_knockback
+	effect.knockback_direction = Vector2.RIGHT.rotated(rotation)
+	effect.rotation_degrees = -45
+	effect.position = Vector2(15, -15)
+	weapon_sprite.add_child(effect)
---
+++ test/Autoloads/saved_data/data_test.gd
@@ -0,0 +1,22 @@
+class_name DataTest
+extends GdUnitTestSuite
+@warning_ignore('unused_parameter')
+@warning_ignore('return_value_discarded')
+
+const __source: String = 'res://Autoloads/saved_data/data.gd'
+
+
+func test_discovered_weapons() -> void:
+	var data: Data = Data.new()
+
+	for weapon_path: String in data.discovered_weapons:
+		assert_bool(FileAccess.file_exists(weapon_path))
+		assert_bool(weapon_path.get_extension() == "tscn")
+
+
+func test_discovered_armors() -> void:
+	var data: Data = Data.new()
+
+	for armor_path: String in data.discovered_armors:
+		assert_bool(FileAccess.file_exists(armor_path))
+		assert_bool(armor_path.get_extension() == "gd")
