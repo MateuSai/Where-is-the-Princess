d2e6890b2464b0d1f89d5e5d8ffa8a1e15f67ffc
Mateu <57108338+MateuSai@users.noreply.github.com>
2024-01-27 07:26:26 +0100

    mercenary sprites

--- Characters/npcs/mercenary/mercenary_spawner.gd
+++ Characters/npcs/mercenary/mercenary_spawner.gd
@@ -3,7 +3,7 @@ class_name MercenarySpawner extends Marker2D
 
 func _ready() -> void:
 	if randi() % 2 == 0:
-		var mercenary: StaticBody2D = (load("res://Characters/npcs/mercenary/RichardTheGigachadWizard.tscn") as PackedScene).instantiate()
+		var mercenary: StaticBody2D = (load("res://Characters/npcs/mercenary/mercenary.tscn") as PackedScene).instantiate()
 		mercenary.position = position
 		get_parent().call_deferred("add_child", mercenary)
 
