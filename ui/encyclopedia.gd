ba213759a0de5f8b26efa2fe916d94032f388698
Mateu <57108338+MateuSai@users.noreply.github.com>
2024-01-30 15:56:33 +0100

    Armor details on encyclopedia

--- ui/encyclopedia.gd
+++ ui/encyclopedia.gd
@@ -9,6 +9,7 @@ enum {
 
 @onready var category_buttons: VBoxContainer = $HBoxContainer/CategoryButtons
 @onready var flow_container: HFlowContainer = $HBoxContainer/ScrollContainer/HFlowContainer
+@onready var details_vbox: VBoxContainer = $HBoxContainer/DetailsScrollContainer/VBoxContainer
 
 
 func _ready() -> void:
@@ -37,11 +38,40 @@ func _set_category(new_category: int) -> void:
 			var discovered_armor_paths: PackedStringArray = SavedData.get_discovered_armors_paths()
 			for armor_path: String in SavedData.get_all_armor_paths():
 				var armor: Armor = load(armor_path).new()
-				var icon: TextureRect = TextureRect.new()
-				icon.texture = armor.get_icon()
+				var button: Button = Button.new()
+				button.icon = armor.get_icon()
 				if not discovered_armor_paths.has(armor_path):
-					icon.modulate = Color.BLACK
-				flow_container.add_child(icon)
+					button.modulate = Color.BLACK
+					button.disabled = true
+				else:
+					button.pressed.connect(func() -> void:
+						for i: int in range(details_vbox.get_child_count() - 1, -1, -1):
+							details_vbox.get_child(i).free()
+
+						var armor_texture: TextureRect = TextureRect.new()
+						armor_texture.expand_mode = TextureRect.EXPAND_FIT_WIDTH
+						armor_texture.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT
+						armor_texture.custom_minimum_size.y = 64
+						var atlas: AtlasTexture = AtlasTexture.new()
+						atlas.atlas = armor.get_sprite_sheet()
+						atlas.region = Rect2(0, 0, 16, 16)
+						armor_texture.texture = atlas
+						details_vbox.add_child(armor_texture)
+
+						var name_label: Label = Label.new()
+						name_label.custom_minimum_size.x = details_vbox.size.x - 16
+						name_label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+						name_label.text = armor.get_armor_name()
+						details_vbox.add_child(name_label)
+
+						var description_label: Label = Label.new()
+						description_label.theme = load("res://SmallFontTheme.tres")
+						description_label.custom_minimum_size.x = details_vbox.size.x - 16
+						description_label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+						description_label.text = armor.get_description()
+						details_vbox.add_child(description_label)
+					)
+				flow_container.add_child(button)
 		ITEMS:
 			var discovered_item_paths: PackedStringArray = SavedData.get_discovered_all_items_paths()
 			for item_path: String in SavedData.get_all_items_paths():
