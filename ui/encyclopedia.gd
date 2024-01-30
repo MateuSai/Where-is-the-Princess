8d5d51138a88c9a080d0370567a1837a01e65558
Mateu <57108338+MateuSai@users.noreply.github.com>
2024-01-30 17:46:07 +0100

    armor condition and ability on encyclopedia

--- ui/encyclopedia.gd
+++ ui/encyclopedia.gd
@@ -48,28 +48,7 @@ func _set_category(new_category: int) -> void:
 						for i: int in range(details_vbox.get_child_count() - 1, -1, -1):
 							details_vbox.get_child(i).free()
 
-						var armor_texture: TextureRect = TextureRect.new()
-						armor_texture.expand_mode = TextureRect.EXPAND_FIT_WIDTH
-						armor_texture.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT
-						armor_texture.custom_minimum_size.y = 64
-						var atlas: AtlasTexture = AtlasTexture.new()
-						atlas.atlas = armor.get_sprite_sheet()
-						atlas.region = Rect2(0, 0, 16, 16)
-						armor_texture.texture = atlas
-						details_vbox.add_child(armor_texture)
-
-						var name_label: Label = Label.new()
-						name_label.custom_minimum_size.x = details_vbox.size.x - 16
-						name_label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
-						name_label.text = armor.get_armor_name()
-						details_vbox.add_child(name_label)
-
-						var description_label: Label = Label.new()
-						description_label.theme = load("res://SmallFontTheme.tres")
-						description_label.custom_minimum_size.x = details_vbox.size.x - 16
-						description_label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
-						description_label.text = armor.get_description()
-						details_vbox.add_child(description_label)
+						_show_armor_details(armor)
 					)
 				flow_container.add_child(button)
 		ITEMS:
@@ -88,3 +67,53 @@ func _set_category(new_category: int) -> void:
 				var icon: TextureRect = TextureRect.new()
 				icon.texture = load("res://Art/Dust.png")
 				flow_container.add_child(icon)
+
+
+func _show_armor_details(armor: Armor) -> void:
+	var top_hbox: HBoxContainer = HBoxContainer.new()
+	var left_vbox: VBoxContainer = VBoxContainer.new()
+	top_hbox.add_child(left_vbox)
+
+	var ability_icon: TextureRect = TextureRect.new()
+	ability_icon.texture = armor.ability_icon if armor.ability_icon else load("res://Art/16x16 Pixel Art Roguelike (Forest) Pack/ui/armor_no_ability.png")
+	left_vbox.add_child(ability_icon)
+
+	var condition_hbox: HBoxContainer = HBoxContainer.new()
+	condition_hbox.alignment = BoxContainer.ALIGNMENT_CENTER
+	condition_hbox.add_theme_constant_override("separation", -1)
+	left_vbox.add_child(condition_hbox)
+
+	var armor_amount_label: Label = Label.new()
+	armor_amount_label.text = str(armor.max_condition)
+	condition_hbox.add_child(armor_amount_label)
+
+	var armor_condition_icon: TextureRect = TextureRect.new()
+	armor_condition_icon.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT
+	armor_condition_icon.texture = load("res://Art/16x16 Pixel Art Roguelike (Forest) Pack/items/armor_shard.png")
+	condition_hbox.add_child(armor_condition_icon)
+
+	var armor_texture: TextureRect = TextureRect.new()
+	armor_texture.expand_mode = TextureRect.EXPAND_FIT_WIDTH
+	armor_texture.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT
+	armor_texture.custom_minimum_size.y = 64
+	var atlas: AtlasTexture = AtlasTexture.new()
+	atlas.atlas = armor.get_sprite_sheet()
+	atlas.region = Rect2(0, 0, 16, 16)
+	armor_texture.texture = atlas
+	top_hbox.add_child(armor_texture)
+
+	details_vbox.add_child(top_hbox)
+
+
+	var name_label: Label = Label.new()
+	name_label.custom_minimum_size.x = details_vbox.size.x - 16
+	name_label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+	name_label.text = armor.get_armor_name()
+	details_vbox.add_child(name_label)
+
+	var description_label: Label = Label.new()
+	description_label.theme = load("res://SmallFontTheme.tres")
+	description_label.custom_minimum_size.x = details_vbox.size.x - 16
+	description_label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+	description_label.text = armor.get_description()
+	details_vbox.add_child(description_label)
