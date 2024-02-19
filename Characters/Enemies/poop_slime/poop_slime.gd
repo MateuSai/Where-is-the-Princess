38fe6cadd331176d87b4a350727e92eb3e987c54
Mateu <57108338+MateuSai@users.noreply.github.com>
2024-02-19 18:04:24 +0100

    poop slime spawns insecton musk when he dies

--- Characters/Enemies/poop_slime/poop_slime.gd
+++ Characters/Enemies/poop_slime/poop_slime.gd
@@ -5,9 +5,15 @@ const POOP_PUDLE_SCENE: PackedScene = preload("res://Characters/Enemies/poop_sli
 
 
 func _ready() -> void:
+	super()
+
 	life_component.died.connect(func() -> void:
-		for i: int in randi() % 3 + 2:
+		for i: int in ((randi() % 3) + 2):
 			var poop_puddle: Node2D = POOP_PUDLE_SCENE.instantiate()
 			poop_puddle.global_position = global_position + Vector2(randf_range(-8, 8), randf_range(-8, 8))
-			get_tree().current_scene.add_child(poop_puddle)
+			get_tree().current_scene.call_deferred("add_child", poop_puddle)
+
+		var insecton_musk: InsectonMusk = load(Globals.ENEMIES["insecton_musk"].path).instantiate()
+		insecton_musk.position = position
+		room.add_enemy(insecton_musk)
 	)
