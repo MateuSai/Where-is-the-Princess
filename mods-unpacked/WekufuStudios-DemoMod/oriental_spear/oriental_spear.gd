97788406be392ed614aeb2f80bc85e34dc1fe2af
Mateu <57108338+MateuSai@users.noreply.github.com>
2024-02-21 18:51:22 +0100

    lowered oriental spear height

--- mods-unpacked/WekufuStudios-DemoMod/oriental_spear/oriental_spear.gd
+++ mods-unpacked/WekufuStudios-DemoMod/oriental_spear/oriental_spear.gd
@@ -10,5 +10,5 @@ func _spawn_ground_attack() -> void:
 	for i: int in amount:
 		var ground_attack: Hitbox = GROUND_ATTACK_SCENE.instantiate()
 		ground_attack.damage = data.ability_damage
-		ground_attack.global_position = spear_point.global_position + Vector2.RIGHT.rotated((2*PI / amount) * i) * randf_range(20, 36)
+		ground_attack.global_position = spear_point.global_position + Vector2.RIGHT.rotated((2*PI / amount) * (i + randf_range(-0.2, 0.2))) * randf_range(20, 36)
 		get_tree().current_scene.add_child(ground_attack)
