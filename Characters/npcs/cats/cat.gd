2a82b0d4ed4b0d4ca249d1c0be4d999af161ecd6
Mateu <57108338+MateuSai@users.noreply.github.com>
2024-02-27 17:21:45 +0100

    2 new kojirou sounds

--- Characters/npcs/cats/cat.gd
+++ Characters/npcs/cats/cat.gd
@@ -1,7 +1,7 @@
 @icon("res://Art/cat_black_0.png")
 class_name Cat extends NPC
 
-const CAT_SOUNDS: PackedStringArray = ["res://Audio/Sounds/cat/小次郎/1.wav", "res://Audio/Sounds/cat/小次郎/2.wav", "res://Audio/Sounds/cat/小次郎/3.wav"]
+const CAT_SOUNDS: PackedStringArray = ["res://Audio/Sounds/cat/小次郎/1.wav", "res://Audio/Sounds/cat/小次郎/2.wav", "res://Audio/Sounds/cat/小次郎/3_amplified.wav", "res://Audio/Sounds/cat/小次郎/5_amplified.wav", "res://Audio/Sounds/cat/小次郎/6_amplified.wav"]
 
 var interacted_with: bool = false
 
