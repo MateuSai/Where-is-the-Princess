8422611957cb6bdbd2752c5cb5d90d7f010ad9f2
Mateu <mateu1502@gmail.com>
2024-03-31 09:15:21 +0200

    sewer encyclopedia background

---
+++ Rooms/Biomes/sewer/sewer.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="full tilemap sewers" tilewidth="16" tileheight="16" tilecount="348" columns="29">
+ <image source="../../../Art/16x16 Pixel Art Roguellike Sewer Pack/tileset/full tilemap sewers.png" width="464" height="192"/>
+</tileset>
