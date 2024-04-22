777d82a09b9a66c7ac83f5b271b8f644f1542ca7
Mateu <mateu1502@gmail.com>
2024-04-22 19:41:47 +0200

    tilese ocludders

---
+++ Rooms/Biomes/crates/village_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="village_tileset" tilewidth="16" tileheight="16" tilecount="576" columns="36">
+ <image source="../../../Art/16x16 Pixel Art Roguelike (Village) Pack/tilesets/full tilemap village.png" width="576" height="256"/>
+</tileset>
