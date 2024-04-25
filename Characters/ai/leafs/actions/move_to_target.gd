c47b3f22cca53b3239f6453667390e4b5f294cd4
Mateu <mateu1502@gmail.com>
2024-04-25 15:58:11 +0200

    avances ai footman and crossbowman

--- Characters/ai/leafs/actions/move_to_target.gd
+++ Characters/ai/leafs/actions/move_to_target.gd
@@ -5,12 +5,11 @@ class_name MoveToTarget extends ActionLeaf
 func tick(actor: Node, _blackboard: Blackboard) -> int:
     actor.move_to_target()
     actor.move()
-    if actor.mov_direction.y >= 0 and actor.animation_player.current_animation != "move":
-        actor.animation_player.play("move")
-    elif actor.mov_direction.y < 0 and actor.animation_player.current_animation != "move_up":
-        actor.animation_player.play("move_up")
 
     if ((actor as Enemy).target.global_position - (actor as Enemy).global_position).length() < closer_than:
         return SUCCESS
 
     return RUNNING
+
+func after_run(actor: Node, _blackboard: Blackboard) -> void:
+    (actor as Enemy).mov_direction = Vector2.ZERO
