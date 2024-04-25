73fbc45eca0643609673cd77ff201ae1bae345e7
Mateu <mateu1502@gmail.com>
2024-04-25 19:55:47 +0200

    removed projectile detection from footman and crossbowman and fixed cat tween bug

--- Characters/npcs/cats/cat.gd
+++ Characters/npcs/cats/cat.gd
@@ -7,7 +7,6 @@ var interacted_with: bool = false
 
 @onready var audio_stream_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
-
 func _on_player_interacted() -> void:
 	audio_stream_player.stream = load(CAT_SOUNDS[randi() % CAT_SOUNDS.size()])
 	audio_stream_player.play()
@@ -17,16 +16,15 @@ func _on_player_interacted() -> void:
 		SavedData.run_stats.luck += 1
 		_spawn_heart()
 
-
 func _spawn_heart() -> void:
 	var heart: Sprite2D = Sprite2D.new()
 	heart.texture = load("res://Art/cat_interaction_UI.png")
-	heart.position = Vector2(-8, -12)
+	heart.position = Vector2( - 8, -12)
 	add_child(heart)
 
 	var t: Tween = create_tween()
 	t.set_parallel(true)
 	t.tween_property(heart, "position:y", heart.position.y - 8, 0.7).set_delay(0.4)
 	t.tween_property(heart, "modulate:a", 0.0, 0.7).set_delay(0.4)
-	await t.finished
+	t.chain()
 	t.tween_callback(heart.queue_free)
