32a12c93904f614b513824c57dec7b847a238aba
Mateu <mateu1502@gmail.com>
2024-04-25 19:49:07 +0200

    footman behavior

--- Characters/ai/leafs/actions/move_to_target.gd
+++ Characters/ai/leafs/actions/move_to_target.gd
@@ -1,15 +1,37 @@
 class_name MoveToTarget extends ActionLeaf
 
+var timer: Timer
+
 @export var closer_than: int = 8
+@export var time_limit: float = -1
+
+func _ready() -> void:
+	if time_limit > 0:
+		timer = Timer.new()
+		timer.one_shot = true
+		add_child(timer)
 
 func tick(actor: Node, _blackboard: Blackboard) -> int:
-    actor.move_to_target()
-    actor.move()
+	if time_limit != - 1 and timer.is_stopped():
+		return SUCCESS
 
-    if ((actor as Enemy).target.global_position - (actor as Enemy).global_position).length() < closer_than:
-        return SUCCESS
+	actor.move_to_target()
+	actor.move()
 
-    return RUNNING
+	if ((actor as Enemy).target.global_position - (actor as Enemy).global_position).length() < closer_than:
+		if actor.mov_direction.y >= 0 and actor.animation_player.current_animation != "idle":
+			actor.animation_player.play("idle")
+		elif actor.mov_direction.y < 0 and actor.animation_player.current_animation != "idle_up":
+			actor.animation_player.play("idle_up")
+
+		return SUCCESS
+
+	return RUNNING
+
+func before_run(_actor: Node, _blackboard: Blackboard) -> void:
+	if time_limit > 0:
+		timer.wait_time = time_limit
+		timer.start()
 
 func after_run(actor: Node, _blackboard: Blackboard) -> void:
-    (actor as Enemy).mov_direction = Vector2.ZERO
+	(actor as Enemy).mov_direction = Vector2.ZERO
--- Characters/ai/leafs/actions/weapon_normal_attack.gd
+++ Characters/ai/leafs/actions/weapon_normal_attack.gd
@@ -2,9 +2,22 @@ class_name WeaponNormalAttack extends ActionLeaf
 
 @onready var enemy_weapons: EnemyWeapons = owner.get_node("EnemyWeapons")
 
-func tick(_actor: Node, _blackboard: Blackboard) -> int:
-    if enemy_weapons.is_busy():
-        return RUNNING
+var attacking: bool = false
+var just_finished_attack: bool = false
 
-    enemy_weapons.attack()
-    return SUCCESS
+func tick(_actor: Node, _blackboard: Blackboard) -> int:
+    if just_finished_attack:
+        just_finished_attack = false
+        return SUCCESS
+
+    if not attacking:
+        attacking = true
+        enemy_weapons.current_weapon.animation_player.animation_finished.connect(_on_attack_animation_ended)
+        enemy_weapons.attack()
+
+    return RUNNING
+
+func _on_attack_animation_ended(_anim_name: String) -> void:
+    attacking = false
+    just_finished_attack = true
+    enemy_weapons.current_weapon.animation_player.animation_finished.disconnect(_on_attack_animation_ended)
