44a59e29bae196ea7b048cfa2a16364f9a82c9d5
Mateu <mateu1502@gmail.com>
2024-04-26 20:05:45 +0200

    some things

--- Characters/ai/leafs/actions/point_weapon_to_target.gd
+++ Characters/ai/leafs/actions/point_weapon_to_target.gd
@@ -1,8 +1,12 @@
 class_name PointWeaponToTarget extends ActionLeaf
 
+@onready var aim_component: AimComponent = owner.get_node_or_null("AimComponent")
 @onready var enemy_weapons: EnemyWeapons = owner.get_node("EnemyWeapons")
 
 func tick(actor: Node, _blackboard: Blackboard) -> int:
-    enemy_weapons.move(((actor as Enemy).target.global_position - (actor as Enemy).global_position).normalized())
+    if aim_component:
+        enemy_weapons.move(aim_component.get_dir().dir)
+    else:
+        enemy_weapons.move(((actor as Enemy).target.global_position - (actor as Enemy).global_position).normalized())
 
     return SUCCESS
