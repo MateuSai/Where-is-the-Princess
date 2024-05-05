56a4be0c51ad2969017e4dc322ff76ff6801ccf3
Mateu <mateu1502@gmail.com>
2024-05-05 08:56:24 +0200

    dfdsfs

--- Weapons/Ranged/Bows/Bow.gd
+++ Weapons/Ranged/Bows/Bow.gd
@@ -1,11 +1,10 @@
 class_name Bow extends BowOrCrossbowWeapon
 
-
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_attack") and not animation_player.is_playing():
 		_charge()
 	elif event.is_action_released("ui_attack"):
-		if animation_player.is_playing() and animation_player.current_animation.begins_with("charge") and is_charging() and weapon_sprite.frame > 0:
+		if animation_player.is_playing() and get_current_animation().begins_with("charge") and is_charging() and weapon_sprite.frame > 0:
 			_bow_attack(animation_player.current_animation_position / animation_player.current_animation_length)
 		elif is_charging() and not animation_player.is_playing():
 			_bow_attack(1.0)
@@ -14,7 +13,6 @@ func _unhandled_input(event: InputEvent) -> void:
 
 	if event.is_action_pressed("ui_weapon_ability") and has_active_ability() and not is_busy() and can_active_ability():
 		_active_ability()
-
 
 ## Charge has a value between 0 and 1 where 1 is max charged
 func _bow_attack(charge: float) -> void:
