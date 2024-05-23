f277587753af1e78f24bf98c54a7e9c01a26be9a
Mateu <mateu1502@gmail.com>
2024-05-23 20:06:32 +0200

    dsfsdfsdf

---
+++ Rooms/Furniture and Traps/traps/window_villagers/window_villager.gd
@@ -0,0 +1,45 @@
+@tool
+
+class_name WindowVillager extends Sprite2D
+
+const POSSIBLE_PROJECTILES: Array[PackedScene] = [ preload ("res://Weapons/projectiles/small_rock/small_rock.tscn")]
+
+const PROJECTILE_SPEED: int = 100
+
+@export var row: int = 0:
+    set(new_row):
+        row = new_row
+        frame_coords.y = row
+
+@onready var room: DungeonRoom = owner
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var throw_position: Marker2D = $ThrowPosition
+@onready var cooldown_timer: Timer = $CooldownTimer
+
+func _ready() -> void:
+    room.player_entered.connect(func() -> void:
+        animation_player.animation_finished.connect(_on_animation_finished)
+        cooldown_timer.timeout.connect(func() -> void:
+            animation_player.play("throw")
+        )
+        cooldown_timer.start(randf_range(2.0, 4.0))
+    )
+    room.cleared.connect(queue_free)
+
+func _on_animation_finished(_anim_name: String) -> void:
+    cooldown_timer.start(randf_range(0.8, 5.0))
+
+func _throw() -> void:
+    var dis: Vector2 = Globals.player.global_position - throw_position.global_position
+
+    var projectile: Projectile = POSSIBLE_PROJECTILES[randi() % POSSIBLE_PROJECTILES.size()].instantiate()
+    projectile.z_index += 1
+    get_tree().current_scene.add_child(projectile)
+    projectile.launch(throw_position.global_position, dis.normalized(), PROJECTILE_SPEED)
+
+    var projectile_lifetime: Timer = Timer.new()
+    projectile_lifetime.one_shot = true
+    projectile.add_child(projectile_lifetime)
+    projectile_lifetime.timeout.connect(projectile.destroy)
+    projectile_lifetime.start(dis.length() / PROJECTILE_SPEED)
