e527f7ac61431032e7c7b5160ef0207f0fd8c22e
Mateu <mateu1502@gmail.com>
2024-05-27 18:28:57 +0200

    new village chest room

--- Autoloads/saved_data/biome_conf.gd
+++ Autoloads/saved_data/biome_conf.gd
@@ -43,6 +43,7 @@ var vertical_corridor_symmetric_lights: bool = false
 
 var floor_tiles_coord: Array[Array] = []
 var corridor_floor_tiles_coor: Array[Array] = []
+#var water_tiles_coor: Array[Array] = []
 
 var default_num_combat_rooms: int = DEFAULT_NUM_COMBAT_ROOMS
 var default_num_chest_rooms: int = DEFAULT_NUM_CHEST_ROOMS
@@ -77,6 +78,10 @@ static func from_dic(dic: Dictionary) -> BiomeConf:
 					data.corridor_floor_tiles_coor = []
 					var a: Array = dic[key]
 					data.corridor_floor_tiles_coor.assign(a)
+				#"water_tiles_coor":
+				#	data.water_tiles_coor = []
+				#	var a: Array = dic[key]
+				#	data.water_tiles_coor.assign(a)
 				"weather_modificators":
 					for weather_modificator_path: String in dic.weather_modificators:
 						data.weather_modificators.push_back(load(weather_modificator_path).new())
