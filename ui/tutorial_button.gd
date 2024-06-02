0b33d9ae905410b88a739224f0265a210bbb6cca
Mateu <mateu1502@gmail.com>
2024-06-02 09:09:18 +0200

    fix #207

--- ui/tutorial_button.gd
+++ ui/tutorial_button.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 		SavedData.run_stats.add_permanent_passive_item(Float.new())
 		SavedData.change_biome_by_id_or_path("tutorial_forest")
 		SceneTransistor.start_transition_to("res://Game.tscn")
-	)
+	, CONNECT_ONE_SHOT)
