33633db997bbc9e8b73990f69ea4ccf8b5f3143c
Mateu <mateu1502@gmail.com>
2024-06-05 19:26:26 +0200

    close #212

--- Autoloads/SceneTransistor.gd
+++ Autoloads/SceneTransistor.gd
@@ -6,11 +6,11 @@ var new_scene: String
 
 @onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
 
-
-func start_transition_to(path_to_scene: String) -> void:
+func start_transition_to(path_to_scene: String, skip_fade_in: bool=false) -> void:
 	new_scene = path_to_scene
 	animation_player.play("change_scene_to_file")
-
+	if skip_fade_in:
+		animation_player.seek(0.45)
 
 func change_scene_to_file() -> void:
 	#print_debug("Changing scene...")
@@ -18,4 +18,3 @@ func change_scene_to_file() -> void:
 	assert(ok)
 	scene_changed.emit(new_scene)
 	#print_debug("Scene changed!")
-
