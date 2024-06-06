b973dcf576ac37c4327bb39eea038fa3da30d91f
Mateu <mateu1502@gmail.com>
2024-06-06 20:05:19 +0200

    ssfsdfd

---
+++ ui/cinematics/intro/royal_carriage.gd
@@ -0,0 +1,7 @@
+class_name RoyalCarriage extends Node2D
+
+@onready var wheels: Node2D = $Wheels
+
+func _physics_process(delta: float) -> void:
+    for wheel: Sprite2D in wheels.get_children():
+        wheel.rotation = wrap(wheel.rotation - 0.4 * delta, 0.0, 2 * PI)
