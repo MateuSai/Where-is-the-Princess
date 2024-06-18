98470acc4798d348d3c11f6f8e11d6e9a941fbc2
Mateu <mateu1502@gmail.com>
2024-06-18 16:04:47 +0200

    fixed crow animation

--- Characters/ai/leafs/actions/update_animation_according_to_mov_direction.gd
+++ Characters/ai/leafs/actions/update_animation_according_to_mov_direction.gd
@@ -3,13 +3,13 @@ class_name UpdateAnimationAccordingToMovdirection extends ActionLeaf
 func tick(actor: Node, _blackboard: Blackboard) -> int:
 	if (actor as Enemy).mov_direction.length() > 0.05:
 		if (actor.animation_player as AnimationPlayer).has_animation("move_left"):
-			if actor.mov_direction.y >= 0 and actor.mov_direction.y > actor.mov_direction.x and actor.animation_player.current_animation != "move":
+			if actor.mov_direction.y >= 0 and abs(actor.mov_direction.y) > abs(actor.mov_direction.x) and actor.animation_player.current_animation != "move":
 				actor.animation_player.play("move")
-			elif actor.mov_direction.y < 0 and actor.mov_direction.y > actor.mov_direction.x and actor.animation_player.current_animation != "move_up":
+			elif actor.mov_direction.y < 0 and abs(actor.mov_direction.y) > abs(actor.mov_direction.x) and actor.animation_player.current_animation != "move_up":
 				actor.animation_player.play("move_up")
-			elif actor.mov_direction.x >= 0 and actor.mov_direction.x > actor.mov_direction.y and actor.animation_player.current_animation != "move_right":
+			elif actor.mov_direction.x >= 0 and abs(actor.mov_direction.x) > abs(actor.mov_direction.y) and actor.animation_player.current_animation != "move_right":
 				actor.animation_player.play("move_right")
-			elif actor.mov_direction.x < 0 and actor.mov_direction.x > actor.mov_direction.y and actor.animation_player.current_animation != "move_left":
+			elif actor.mov_direction.x < 0 and abs(actor.mov_direction.x) > abs(actor.mov_direction.y) and actor.animation_player.current_animation != "move_left":
 				actor.animation_player.play("move_left")
 		else:
 			if actor.mov_direction.y >= 0 and actor.animation_player.current_animation != "move":
