8b458190023d1352631133cd0d965129d59a1b9c
Mateu <mateu1502@gmail.com>
2024-06-26 15:09:24 +0200

    stamina bar grows

--- Characters/Player/stamina_bar.gd
+++ Characters/Player/stamina_bar.gd
@@ -1,17 +1,19 @@
 extends TextureProgressBar
 
+## With a scale of 0.3 and a max stamina of 100, the width would be 0.3 * 100 = 30 pixels
+const SCALE: float = 0.59
 
 @onready var player: Player = owner
+@onready var nine_patch: NinePatchRect = get_node("%StaminaNinePatch")
 @onready var update_timer: Timer = $UpdateTimer
-
 
 func _ready() -> void:
 	update_timer.timeout.connect(_update)
 
 	player.max_stamina_changed.connect(func(new_value: float) -> void:
-		max_value = new_value
+		max_value=new_value
+		nine_patch.custom_minimum_size.x=max_value * SCALE
 	)
-
 
 func _update() -> void:
 	value = player.stamina
