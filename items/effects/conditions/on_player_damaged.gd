81fae0bb8092c30190f6e43df99fd9a0d939da41
Mateu <mateu1502@gmail.com>
2024-07-11 19:19:07 +0200

    sdfsdf

--- items/effects/conditions/on_player_damaged.gd
+++ items/effects/conditions/on_player_damaged.gd
@@ -3,26 +3,26 @@ class_name OnPlayerDamaged extends ItemEffectsCondition
 var _ranged_only: bool
 
 func _init(effects: Array[ItemEffect]=[], enable_times_limit: int=- 1, ranged_only: bool=false) -> void:
-    super(effects, enable_times_limit)
-    self._ranged_only = ranged_only
+	super(effects, enable_times_limit)
+	self._ranged_only = ranged_only
 
 @warning_ignore("shadowed_variable")
 func enable(player: Player) -> void:
-    super(player)
+	super(player)
 
-    player.life_component.damage_taken.connect(_on_player_damage_taked)
+	player.life_component.damage_taken.connect(_on_player_damage_taked)
 
 @warning_ignore("shadowed_variable")
 func disable(player: Player) -> void:
-    super(player)
+	super(player)
 
-    player.life_component.damage_taken.disconnect(_on_player_damage_taked)
+	player.life_component.damage_taken.disconnect(_on_player_damage_taked)
 
 func _on_player_damage_taked(_dam: int, _dir: Vector2, _force: int) -> void:
-    if _ranged_only and not player.life_component.last_is_ranged:
-        return
+	if _ranged_only and not player.life_component.last_is_ranged:
+		return
 
-    _enable_effects()
+	_enable_effects()
 
 func get_description() -> String:
-    return "%s\n%s" % [tr("ON_PLAYER_DAMAGED"), super()]
+	return "%s\n%s" % [tr("ON_PLAYER_DAMAGED"), super()]
--- items/effects/weapons/add_condition_cost_multiplier_by_type.gd
+++ items/effects/weapons/add_condition_cost_multiplier_by_type.gd
@@ -1,15 +1,17 @@
 class_name AddConditionCostMultiplierByType extends ItemEffect
 
-var type: WeaponData.Type
-var amount: float
+var _type: WeaponData.Type
+var _amount: float
 
-@warning_ignore("shadowed_variable")
 func _init(type: WeaponData.Type, amount: float) -> void:
-	self.type = type
-	self.amount = amount
+	_type = type
+	_amount = amount
 
 func enable(player: Player) -> void:
-	player.weapons.add_condition_cost_multiplier_by_type(type, amount)
+	player.weapons.add_condition_cost_multiplier_by_type(_type, _amount)
 
 func disable(player: Player) -> void:
-	player.weapons.add_condition_cost_multiplier_by_type(type, amount)
+	player.weapons.add_condition_cost_multiplier_by_type(_type, _amount)
+
+func get_description() -> String:
+	return _get_color_tag(GREEN if _amount < 0 else RED) % (tr("WEAPON_CONDITION_COST_MULTIPLIER_BY_TYPE") % [str(_amount), tr(WeaponData.Type.keys()[_type].to_snake_case().to_upper())])
