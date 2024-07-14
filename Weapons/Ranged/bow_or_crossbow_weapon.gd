15186fa7cac4ef23617122c588dec14df7f6cf04
Mateu <mateu1502@gmail.com>
2024-07-14 19:24:17 +0200

    bows and crossbows can equip one gem now. close #255

--- Weapons/Ranged/bow_or_crossbow_weapon.gd
+++ Weapons/Ranged/bow_or_crossbow_weapon.gd
@@ -17,4 +17,21 @@ func _spawn_projectile(angle: float = 0.0, amount: int = 1, rotate_to_dir: bool 
 		assert(projectile is ArrowOrBolt)
 		projectile.type = arrow_type
 
+		for status: StatusWeaponModifier in stats.modifiers:
+			@warning_ignore("unsafe_call_argument")
+			var status_inflicter_component: StatusInflicterComponent = projectile.get_node_or_null(StatusComponent.Status.keys()[status.status] + "Inflicter")
+			if status_inflicter_component:
+				status_inflicter_component.change_to_inflict_status_effect += StatusWeaponModifier.INFLICT_CHANCE
+			else:
+				status_inflicter_component = StatusInflicterComponent.new()
+				status_inflicter_component.status = status.status
+				status_inflicter_component.change_to_inflict_status_effect = StatusWeaponModifier.INFLICT_CHANCE * amount
+				status_inflicter_component.name = StatusComponent.Status.keys()[status.status] + "Inflicter"
+				projectile.add_child(status_inflicter_component)
+
 	return spawned_projectiles
+
+
+func add_status_inflicter(status: StatusComponent.Status, amount: int=1) -> void:
+	for i: int in amount:
+		status_inflicter_added.emit(self, status)
