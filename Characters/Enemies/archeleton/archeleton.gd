58ca6dd996c25a2a394b4337f2a194c9e520282f
Mateu <mateu1502@gmail.com>
2024-07-20 18:48:51 +0200

    archeleton drops head

--- Characters/Enemies/archeleton/archeleton.gd
+++ Characters/Enemies/archeleton/archeleton.gd
@@ -4,6 +4,8 @@ const NO_HEAD_SPRITESHEET: Texture2D = preload("res://Art/16x16 Pixel Art Roguel
 
 var projectile_speed: int = 200
 
+var has_head: bool = true
+
 @onready var bow_container: Node2D = $BowContainer
 @onready var bow_sprite: Sprite2D = $BowContainer/Sprite2D
 @onready var aim_component: AimComponent = $AimComponent
@@ -13,7 +15,7 @@ func _ready() -> void:
 
 	life_component.damage_taken.connect(func(dam: int, dir: Vector2, force: int) -> void:
 		if life_component.hp > 0:
-			_remove_head()
+			_remove_head(dam, dir, force)
 	, CONNECT_ONE_SHOT)
 
 
@@ -30,6 +32,16 @@ func _ready() -> void:
 	#get_tree().current_scene.add_child(arrow)
 	#arrow.launch(bow_sprite.global_position, Vector2.RIGHT.rotated(bow_container.rotation), projectile_speed, true)
 
-func _remove_head() -> void:
+func _remove_head(dam: int, dir: Vector2, force: int) -> void:
+	has_head = false
 	sprite.texture = NO_HEAD_SPRITESHEET
 	aim_component.mode = aim_component.RANDOM_AIM
+
+	var skull: Fragment = load("res://effects/fragments/skull.tscn").instantiate()
+	skull.position = position + Vector2.UP * 10
+	room.add_child(skull)
+	var dis: float = force / 10.0
+	skull.throw(self, dir, dis, dis)
+
+	if dam > 4:
+		(skull.get_node("LifeComponent") as LifeComponent).take_damage(dam, dir, force, null, null, "")
--- effects/fragments/fragment.gd
+++ effects/fragments/fragment.gd
@@ -7,7 +7,7 @@ class_name Fragment extends Sprite2D
 func _ready() -> void:
 	assert(not textures.is_empty())
 
-func throw(thrower_body: Node, dir: Vector2) -> void:
+func throw(thrower_body: Node, dir: Vector2, min_dis: float = 18, max_dis: float = 36) -> void:
 	if dir == Vector2.ZERO:
 		dir = Vector2.RIGHT.rotated(randf_range(0, 2 * PI))
 
@@ -15,7 +15,7 @@ func throw(thrower_body: Node, dir: Vector2) -> void:
 	var tween: Tween = create_tween()
 	tween.set_parallel(true)
 	var time: float = 0.5
-	tween.tween_property(self, "position", position + dir * randf_range(18, 36), time).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "position", position + dir * randf_range(min_dis, max_dis), time).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 	tween.tween_property(self, "rotation", [1, -1][randi() % 2] * randf_range(0.8, 5), time).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 
 	area.body_entered.connect(func(body: Node) -> void:
