4e9f015c04fc2f929a84d78897f483dd5c5a9f06
Mateu <mateu1502@gmail.com>
2024-07-23 19:36:24 +0200

    sdfdsfsfs

--- items/effects/conditions/on_hp_less_than.gd
+++ items/effects/conditions/on_hp_less_than.gd
@@ -1,29 +1,32 @@
 class_name OnHpLessThan extends ItemEffectsCondition
 
-var less_than: int
+var _less_than: int
 
 @warning_ignore("shadowed_variable")
 func _init(less_than: int, effects: Array[ItemEffect]) -> void:
-    self.less_than = less_than
-    
-    super(effects)
+	_less_than = less_than
+
+	super(effects)
 
 @warning_ignore("shadowed_variable")
 func enable(player: Player) -> void:
-    super(player)
-    
-    player.life_component.hp_changed.connect(_on_hp_changed)
-    
-    _on_hp_changed(player.life_component.hp)
+	super(player)
+
+	player.life_component.hp_changed.connect(_on_hp_changed)
+
+	_on_hp_changed(player.life_component.hp)
 
 @warning_ignore("shadowed_variable")
 func disable(player: Player) -> void:
-    super(player)
+	super(player)
 
-    player.life_component.hp_changed.disconnect(_on_hp_changed)
+	player.life_component.hp_changed.disconnect(_on_hp_changed)
 
 func _on_hp_changed(new_hp: int) -> void:
-    if new_hp < less_than and not in_effect:
-        _enable_effects()
-    elif in_effect and new_hp >= less_than:
-        _disable_effects()
+	if new_hp < _less_than and not in_effect:
+		_enable_effects()
+	elif in_effect and new_hp >= _less_than:
+		_disable_effects()
+
+func get_description() -> String:
+	return "%s\n%s" % [tr("ON_HP_LESS_THAN") % str(_less_than), super()]
