e6cf17c3c942c81769dc762ee0af703a510adfdc
Mateu <mateu1502@gmail.com>
2024-08-01 10:25:19 +0200

    dsfdsf

--- ui/armor_point_ui.gd
+++ ui/armor_point_ui.gd
@@ -1,17 +1,24 @@
 class_name ArmorPointUI extends TextureRect
 
-const TEXTURES: Array[Texture2D] = [preload("res://Art/16x16 Pixel Art Roguelike (Forest) Pack/ui/Armor_UI_zero.png"), preload("res://Art/16x16 Pixel Art Roguelike (Forest) Pack/ui/Armor_UI_full.png")]
+const TEXTURES: Array[Array] = [
+	[preload("res://Art/16x16 Pixel Art Roguelike (Forest) Pack/ui/Armor_UI_bit_empty.png"), preload("res://Art/16x16 Pixel Art Roguelike (Forest) Pack/ui/Armor_UI_bit_full.png")],
+
+	[preload("res://Art/16x16 Pixel Art Roguelike (Forest) Pack/ui/Armor_UI_medium_empty.png"), preload("res://Art/16x16 Pixel Art Roguelike (Forest) Pack/ui/Armor_UI_medium_half.png"), preload("res://Art/16x16 Pixel Art Roguelike (Forest) Pack/ui/Armor_UI_medium_full.png")],
+
+	[preload("res://Art/16x16 Pixel Art Roguelike (Forest) Pack/ui/Armor_UI_zero.png"), preload("res://Art/16x16 Pixel Art Roguelike (Forest) Pack/ui/Armor_UI_bit.png"), preload("res://Art/16x16 Pixel Art Roguelike (Forest) Pack/ui/Armor_UI_medium.png"), preload("res://Art/16x16 Pixel Art Roguelike (Forest) Pack/ui/Armor_UI_full.png")]
+]
 
 
 var ap: int:
 	set(new_ap):
-		assert(new_ap >= 0 and new_ap <= 1)
+		assert(new_ap >= 0 and new_ap <= 3)
 		ap = new_ap
-		texture = TEXTURES[ap]
+		texture = TEXTURES[max_ap - 1][ap]
+var max_ap: int
 
 
 func _init() -> void:
 	expand_mode = TextureRect.EXPAND_IGNORE_SIZE
 	stretch_mode = TextureRect.STRETCH_TILE
-	custom_minimum_size = Vector2(9, 9)
-	texture = TEXTURES[TEXTURES.size() - 1]
+	custom_minimum_size = Vector2(8, 9)
+	#texture = TEXTURES[TEXTURES.size() - 1]
--- ui/armor_points_HBox.gd
+++ ui/armor_points_HBox.gd
@@ -10,26 +10,31 @@ func update_armor_points(new_ap: int, max_ap: int) -> void:
 	var dif: int = new_ap - previous_ap
 	if dif > 0:
 		var i: int = dif
-		var current_armor_point_ui: ArmorPointUI = get_child(int(previous_ap/1.0)) if get_child_count() >= int(previous_ap/1.0) + 1 else null
+		var current_armor_point_ui: ArmorPointUI = get_child(int(previous_ap/3.0)) if get_child_count() >= int(previous_ap/3.0) + 1 else null
 		while i > 0:
 			if current_armor_point_ui == null:
 				var new_armor_point_ui: ArmorPointUI = ArmorPointUI.new()
+				new_armor_point_ui.max_ap = min(3, i)
 				add_child(new_armor_point_ui)
 				new_armor_point_ui.ap = 0
 				current_armor_point_ui = new_armor_point_ui
-			i -= 1 - current_armor_point_ui.ap
-			current_armor_point_ui.ap = 1 if i >= 0 else 1 + i
+			else:
+				if (current_armor_point_ui.get_index() + 1) * 3 <= new_ap:
+					current_armor_point_ui.max_ap = 3
+			i -= 3 - current_armor_point_ui.ap
+			current_armor_point_ui.ap = 3 if i >= 0 else 3 + i
 			current_armor_point_ui = get_child(current_armor_point_ui.get_index() + 1) if get_child_count()-1 > current_armor_point_ui.get_index() else null
 	elif dif < 0:
 		var i: int = abs(dif)
-		var current_index: int = int((ceil(previous_ap/1.0) - 1.0) as float)
+		var current_index: int = int((ceil(previous_ap/3.0) - 1.0) as float)
 		var current_armor_point_ui: ArmorPointUI = get_child(current_index)
 		while i > 0:
 			assert(current_armor_point_ui)
 			i -= current_armor_point_ui.ap
-			if max_ap == 0 or (current_index * 1) >= max_ap:
+			if max_ap == 0 or (current_index * 3) >= max_ap:
 				current_armor_point_ui.queue_free()
 			else:
+				current_armor_point_ui.max_ap = 3 if (current_index + 1) <= (max_ap / 3.0) else (max_ap % 3)
 				current_armor_point_ui.ap = 0 if i >= 0 else 0 - i
 			current_armor_point_ui = get_child(current_index - 1)
 			current_index -= 1
